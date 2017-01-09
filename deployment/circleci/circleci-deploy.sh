c7dea7b05a67901770b73d7caa29ac33e9d3f8ae
Russell Heimlich <kingkool68@gmail.com>
2017-01-09 12:58:28 -0500

    Don't worry about `object-cache.php`
    We don't version control it anymore. If it needs to be moved it will be done manually.

--- deployment/circleci/circleci-deploy.sh
+++ deployment/circleci/circleci-deploy.sh
@@ -74,9 +74,6 @@ git clone git@github.com:spiritedmedia/spiritedmedia-build.git tmp/
 mv tmp/.git .
 rm -rf tmp/
 
-# Move wp-redis' object-cache.php into place
-mv wp-content/plugins/wp-redis/object-cache.php wp-content/object-cache.php
-
 # If no branch is set then assume master
 if [ ! $CIRCLE_BRANCH  ]; then
 	CIRCLE_BRANCH="master"
