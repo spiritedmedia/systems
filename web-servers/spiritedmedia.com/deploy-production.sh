3886746cc71dc953853ba8bd0df6e0a4461288b4
Russell Heimlich <kingkool68@gmail.com>
2017-05-12 15:05:22 -0400

    Updating deployment script
    1. Uses AWS CLI for syncing instead of S3cmd tool
    2. Flushing permalinks actually works

--- web-servers/spiritedmedia.com/deploy-production.sh
+++ web-servers/spiritedmedia.com/deploy-production.sh
@@ -11,15 +11,73 @@ git reset --hard origin/master
 # Reset file ownership
 chown -R www-data:www-data /var/www/spiritedmedia.com/htdocs/
 
-# Sync static theme files to S3 so they can be served through a CDN
-s3cmd sync \
-	--acl-public \
-	--no-mime-magic \
-	--guess-mime-type \
-	--storage-class REDUCED_REDUNDANCY \
-	--exclude-from /var/www/spiritedmedia.com/scripts/sync.exclude \
-	--include-from /var/www/spiritedmedia.com/scripts/sync.include \
-	/var/www/spiritedmedia.com/htdocs/ s3://spiritedmedia-com/
+# Sync static files to S3 so they can be served through a CDN
+# Helpful article: https://www.lambrospetrou.com/articles/aws-s3-sync-git-status/
+
+# General args go here
+AWS_SYNC_ARGS=(
+  # --dryrun
+  --acl public-read
+  --storage-class REDUCED_REDUNDANCY
+)
+
+# File types and locations to be excluded from sync
+AWS_EXCLUDE=(
+  '*'
+  'composer.json'
+  '.git/*'
+)
+
+# File types and locations to be included from sync
+AWS_INCLUDE=(
+  # CSS/JavaScript
+  '*.css'
+  '*.js'
+  '*.json'
+
+  # Images
+  '*.png'
+  '*.jpg'
+  '*.jpeg'
+  '*.bmp'
+  '*.gif'
+  '*.ico'
+  '*.svg'
+
+  # Fonts
+  '*.woff2'
+  '*.woff'
+  '*.ttf'
+  '*.otf'
+
+  # Other
+  '*.pdf'
+  '*.xml'
+)
+for i in "${AWS_EXCLUDE[@]}"; do
+  AWS_SYNC_ARGS+=('--exclude="'$i'"')
+done
+
+for i in "${AWS_INCLUDE[@]}"; do
+  AWS_SYNC_ARGS+=('--include="'$i'"')
+done
+
+# echo "${AWS_SYNC_ARGS[@]}"
+
+echo "=== SYNCING THEMES DIRECTORY ==="
+echo "${AWS_SYNC_ARGS[@]}" | xargs aws s3 sync /var/www/spiritedmedia.com/htdocs/wp-content/themes/ s3://spiritedmedia-com/wp-content/themes/
+
+echo "=== SYNCING PLUGINS DIRECTORY ==="
+echo "${AWS_SYNC_ARGS[@]}" | xargs aws s3 sync /var/www/spiritedmedia.com/htdocs/wp-content/plugins/ s3://spiritedmedia-com/wp-content/plugins/
+
+echo "=== SYNCING MU-PLUGINS DIRECTORY ==="
+echo "${AWS_SYNC_ARGS[@]}" | xargs aws s3 sync /var/www/spiritedmedia.com/htdocs/wp-content/mu-plugins/ s3://spiritedmedia-com/wp-content/mu-plugins/
+
+echo "=== SYNCING WP-ADMIN DIRECTORY ==="
+echo "${AWS_SYNC_ARGS[@]}" | xargs aws s3 sync /var/www/spiritedmedia.com/htdocs/wp-admin/ s3://spiritedmedia-com/wp-admin/
+
+echo "=== SYNCING WP-ADMIN DIRECTORY ==="
+echo "${AWS_SYNC_ARGS[@]}" | xargs aws s3 sync /var/www/spiritedmedia.com/htdocs/wp-includes/ s3://spiritedmedia-com/wp-includes/
 
 # Flush Redis Cache
 redis-cli -h redis.spiritedmedia.com flushall
@@ -28,4 +86,8 @@ redis-cli -h redis.spiritedmedia.com flushall
 ee stack restart --nginx --php7
 
 # Flush permalinks
-wp rewrite flush --allow-root
+for url in $(wp site list --field=url)
+do
+  echo $url #Used for progress purposes
+  wp rewrite flush --allow-root --url=$url
+done
