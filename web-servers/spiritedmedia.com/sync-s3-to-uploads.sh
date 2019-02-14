bfa3a56b890cfe185adb35b38ce2e7be183a5462
Russell Heimlich <kingkool68@gmail.com>
2019-02-14 11:37:19 -0500

    Wrap comments.

--- web-servers/spiritedmedia.com/sync-s3-to-uploads.sh
+++ web-servers/spiritedmedia.com/sync-s3-to-uploads.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# This script will sync uploaded media stored in our S3 bucket to the server. Use this if you need to pull down uploads from the cloud.
+# This script will sync uploaded media stored in our S3 bucket to the server.
+# Use this if you need to pull down uploads from the cloud.
 # Requires the aws-cli https://aws.amazon.com/cli/
 
 aws s3 sync s3://spiritedmedia-com/wp-content/uploads/ /var/www/spiritedmedia.com/htdocs/wp-content/uploads/
