e3d74362ede4fad4dd9ed63e5d4d3df55da388cd
Chris Montgomery <chris@montchr.io>
2019-02-22 13:52:44 -0500

    Add staging.denverite.com to renew-ssl-certs.sh

--- web-servers/staging.spiritedmedia.com/ssl/renew-ssl-certs.sh
+++ web-servers/staging.spiritedmedia.com/ssl/renew-ssl-certs.sh
@@ -4,7 +4,7 @@
 sudo rm -rf /var/www/staging.spiritedmedia.com/htdocs/.well-known/acme-challenge
 
 # Run Lets Encrpyt
-/opt/letsencrypt/letsencrypt-auto certonly --webroot -w /var/www/staging.spiritedmedia.com/htdocs/ -d staging.spiritedmedia.com -d staging.billypenn.com -d staging.theincline.com --email systems@spiritedmedia.com --text --agree-tos
+/opt/letsencrypt/letsencrypt-auto certonly --webroot -w /var/www/staging.spiritedmedia.com/htdocs/ -d staging.spiritedmedia.com -d staging.billypenn.com -d staging.theincline.com -d staging.denverite.com --email systems@spiritedmedia.com --text --agree-tos
 
 # Restart Nginx
 sudo ee stack reload --nginx
